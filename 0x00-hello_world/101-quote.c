0ebe0433f3cb10744a65833a89d77f5b056b6693
Kobusingye Carolyne <kobusingyec@gmail.com>
2023-02-16 17:26:32 -0800

    print given statement

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#iclude <stdio.h>
+
+/**
+ * main is the entry point
+ * 
+ * returns and that piece of art is useful" - Dora Korpar, 2015-10-19
+ */
+
+ int main(voi) {
+ put(and that piece of art is useful" - Dora Korpar, 2015-10-19):
+ printf("%d\n and that piece of art is useful" - Dora Korpar, 2015-10-19")
+ 
+ return (0):
+ }
