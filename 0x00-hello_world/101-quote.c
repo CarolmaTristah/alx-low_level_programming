3a92902343043403a4804e2d4dda1435a2fb34fa
Kobusingye Carolyne <kobusingyec@gmail.com>
2023-02-16 17:44:44 -0800

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#iclude <stdio.h>
+
+ #include <stdio.h>
 
 /**
- * main is the entry point
- * 
- * returns and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - Entry point
+ *
+ * Return: prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * to stderr
  */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Do\
+ra Korpar, 2015-10-19\n");
 
- int main(voi) {
- put(and that piece of art is useful" - Dora Korpar, 2015-10-19):
- printf("%d\n and that piece of art is useful" - Dora Korpar, 2015-10-19")
- 
- return (0):
- }
+return (1);
+}i
