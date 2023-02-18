1f14d83dc62269522acd9d36344fdb81b1d7bf12
Kobusingye Carolyne <kobusingyec@gmail.com>
2023-02-17 16:34:40 -0800

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,32 +6,29 @@
  */
 int main(void)
 {
-	int a, b, c, d, i;
-
-	i = 0;
-
-	while (i <= 999)
-	{
-	a = i / 100;
-	b = i / 10;
-	c = i % 10;
-	d = b % 10;
-	if ((a >= d) || (a >= c) || (d >= c))
-	{
-	i++;
-	continue;
-	}
-	putchar(a + '0');
-	putchar(d + '0');
-	putchar(c + '0');
-	if (i != 789)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	i++;
-	}
-	putchar('\n');
-	return (0);
+int a, b, c, d, i;
+i = 0;
+while (i <= 999)
+{
+a = i / 100;
+b = i / 10;
+c = i % 10;
+d = b % 10;
+if ((a >= d) || (a >= c) || (d >= c))
+{
+i++;
+continue;
 }
-
+putchar(a + '0');
+putchar(d + '0');
+putchar(c + '0');
+if (i != 789)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
