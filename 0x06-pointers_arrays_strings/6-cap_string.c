a1477f60bdcb741030d8e15622a78f49d533c6ad
CarolmaTristah <kobusingyec@gmail.com>
2023-03-02 17:15:14 -0800

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: string to capitalize
- *
- * Return: pointer to string
- */
+* *cap_string - this is awesome
+* @s: pointer to char params
+*
+* Return: *s
+*/
+
 char *cap_string(char *s)
 {
-int i;
+int i, j;
+char delimeters[] = " \t\n,;.!?\"(){}";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] >= 'a' && s[i] <= 'z')
-{
-if (i == 0)
-s[i] -= 32;
-else if (s[i - 1] == ' ' || s[i - 1] == '\t' || 
-s[i - 1] == '\n' || s[i - 1] == ',' || 
-s[i - 1] == ';' || s[i - 1] == '.' || 
-s[i - 1] == '!' || s[i - 1] == '?' || 
-s[i - 1] == '"' || s[i - 1] == '(' || 
-s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
-s[i] -= 32;
+if (s[0] >= 97 && s[0] <= 122)
+s[0] = s[0] - 32;
+for (j = 0; delimeters[j] != '\0'; j++)
+if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+s[i + 1] = s[i + 1] - 32;
 }
-}
-
 return (s);
 }
