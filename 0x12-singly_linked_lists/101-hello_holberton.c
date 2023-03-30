1977a2a70e4210271d17515c8cf2b50865ce8886
CarolmaTristah <kobusingyec@gmail.com>
2023-03-30 12:50:16 -0700

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * hello_holberton - prints "Hello, Holberton" followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+printf("Hello, Holberton\n");
+return (0);
+}
